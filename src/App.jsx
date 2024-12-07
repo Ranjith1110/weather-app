6f5457299dd8e553a514612fb1ab4d2b102478d5
Ranjith <ranjithram878@gmail.com>
2024-12-07 14:37:14 +0530

    Updated

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import WeatherApp from './components/WeatherApp'
+
+const App = () => {
+  return (
+    <>
+      <div className='app'>
+      <WeatherApp />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/WeatherApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./WeatherApp.css"
+
+const WeatherApp = () => {
+    return (
+        <>
+            <div className=''>
+            <h2>WeatherApp</h2>
+            </div>
+        </>
+    )
+}
+
+export default WeatherApp
