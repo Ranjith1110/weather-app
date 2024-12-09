78fcf93fc1b41abc595c79b413dec420e1ea08ea
Ranjith <ranjithram878@gmail.com>
2024-12-09 10:27:39 +0530

    Updated

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -11,18 +11,18 @@ import wind from '../assets/wind.png'
 
 const WeatherApp = () => {
 
-  const search = async (city)=> {
-    try {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`
+    const search = async (city) => {
+        try {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`
 
-        const response = await fetch(url);
-        const data = await response.json;
-        console.log(data);
+            const response = await fetch(url);
+            const data = await response.json;
+            console.log(data);
 
-    } catch (error) {
-        
+        } catch (error) {
+
+        }
     }
-  }
 
     return (
         <>
